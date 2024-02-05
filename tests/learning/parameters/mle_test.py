19498b6417058d537a9ea0bc8568f6147854b09d
Carlos Li Hu <carloslihu96@gmail.com>
2024-02-05 12:23:33 +0100

    tests reviewed and formatted

--- tests/learning/parameters/mle_test.py
+++ tests/learning/parameters/mle_test.py
@@ -1,5 +1,6 @@
-import pytest
 import numpy as np
+import pytest
+
 import pybnesian as pbn
 import util_test
 
@@ -20,17 +21,22 @@ def numpy_fit_mle_lg(data, variable, evidence):
     N = variable_data.shape[0]
     d = evidence_data.shape[1]
     linregress_data = np.column_stack((np.ones(N), evidence_data.to_numpy()))
-    (beta, res, _, _) = np.linalg.lstsq(linregress_data, variable_data.to_numpy(), rcond=None)
+    (beta, res, _, _) = np.linalg.lstsq(
+        linregress_data, variable_data.to_numpy(), rcond=None
+    )
     var = res / (N - d - 1)
 
     return beta, var
 
+
 def test_mle_create():
     with pytest.raises(ValueError) as ex:
-        mle = pbn.MLE(pbn.CKDEType())
+        pbn.MLE(pbn.CKDEType())
     assert "MLE not available" in str(ex.value)
 
     mle = pbn.MLE(pbn.LinearGaussianCPDType())
+    assert mle is not None
+
 
 def test_mle_lg():
     mle = pbn.MLE(pbn.LinearGaussianCPDType())
