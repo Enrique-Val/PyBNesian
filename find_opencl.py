fdf6d13012bf5cd9aa28875fdc82fa3699a1af42
Carlos Li Hu <carloslihu96@gmail.com>
2024-03-20 18:27:55 +0100

    code formatted

--- find_opencl.py
+++ find_opencl.py
@@ -1,8 +1,18 @@
 import os
 import platform
+
 # This code is based on FindOpenCL.cmake in https://github.com/Kitware/CMake/blob/master/Modules/FindOpenCL.cmake
 
-opencl_env = ["PROGRAMFILES(X86)", "AMDAPPSDKROOT", "INTELOCLSDKROOT", "NVSDKCOMPUTE_ROOT", "CUDA_PATH", "ATISTREAMSDKROOT", "OCL_ROOT"]
+opencl_env = [
+    "PROGRAMFILES(X86)",
+    "AMDAPPSDKROOT",
+    "INTELOCLSDKROOT",
+    "NVSDKCOMPUTE_ROOT",
+    "CUDA_PATH",
+    "ATISTREAMSDKROOT",
+    "OCL_ROOT",
+]
+
 
 def find_files(folder, search_files):
     for s in search_files:
@@ -31,14 +41,19 @@ def find_opencl_include_dir():
 
     return None
 
+
 def find_opencl_library_dir():
     search_files = ["OpenCL.lib"]
 
     if platform.architecture()[0] == "32bit":
         suffix = ["AMD APP/lib/x86", "lib/x86", "lib/Win32", "OpenCL/common/lib/Win32"]
     elif platform.architecture()[0] == "64bit":
-        suffix = ["AMD APP/lib/x86_64", "lib/x86_64", "lib/x64", "OpenCL/common/lib/x64"]
-
+        suffix = [
+            "AMD APP/lib/x86_64",
+            "lib/x86_64",
+            "lib/x64",
+            "OpenCL/common/lib/x64",
+        ]
 
     for env in opencl_env:
         if env in os.environ:
